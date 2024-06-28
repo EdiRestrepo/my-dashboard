7efad5902577ac631ab5e930ed4e427ff9047621
EdiRestrepo <eerestre@bancolombia.com.co>
2024-06-27 21:18:29 -0500

    update Angular 17+

--- src/app/dashboard/pages/user/user.component.ts
+++ src/app/dashboard/pages/user/user.component.ts
@@ -1,11 +1,63 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, computed, inject, signal } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { TitleComponent } from '@shared/title/title.component';
+import { User } from '../../../interfaces/req-response';
+import { toSignal } from '@angular/core/rxjs-interop'
+import { switchMap } from 'rxjs';
+import { UsersService } from '@services/users.service';
 
 @Component({
   standalone: true,
-  imports: [],
-  templateUrl: './user.component.html',
-  styles: ``
+  imports: [CommonModule, TitleComponent],
+  template: `
+    <app-title [title]="titleLabel()"/>
+
+    @if ( user() ) {
+      <section>
+        <img
+          [srcset]="user()?.avatar"
+          [alt]="user()?.first_name"/>
+
+        <div>
+          <h3>{{user()?.first_name}} {{ user()?.last_name}}</h3>
+          <p>{{user()?.email}}</p>
+        </div>
+      </section>
+
+    }@else {
+      <p>Cargando información...</p>
+    }
+
+  `
 })
 export default class UserComponent {
+
+  private route = inject( ActivatedRoute );
+  private usersService = inject( UsersService );
+
+  // public user = signal<User | undefined>(undefined);
+
+  // titleLabel = Información del usuario
+
+
+
+  //todo: el toSignal es una funcion que permite tomar un Observable y devolver una señal
+  public user = toSignal(
+    this.route.params.pipe(
+      switchMap( ({ id }) => this.usersService.getUserById( id ) )
+    )
+  )
+
+  public titleLabel = computed( () => {
+
+    return this.user() ? `Información del usuario: ${this.user()?.first_name} ${this.user()?.last_name}` : 'Información del usuario'
+  })
+
+  // constructor(){
+  //   this.route.params.subscribe(params => {
+  //     console.log({params})
+  //   })
+  // }
 
 }
--- src/app/shared/title/title.component.ts
+++ src/app/shared/title/title.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { booleanAttribute, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   standalone: true,
   imports: [],
-  templateUrl: './title.component.html',
+  template:`
+    <h1 class="text-3xl mb-3.5">{{title}}</h1>
+  `,
 })
 export class TitleComponent {
+
+  @Input({required: true}) title!: string;
+  @Input({ transform: booleanAttribute}) withShadow: boolean = false;
+
+
 
 }
